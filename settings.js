e151df8312ffab8e3bcac046b82c9cb83f2b038f
ebukART <157440147+ebukART@users.noreply.github.com>
2024-06-30 09:26:34 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,22 +17,22 @@ global.location = "Nigeria, Lagos, Africa" //ur location
 
 //new
 global.botname = 'ᗰᗩᏀ€ᑎᎢᗩ' //ur bot name
-global.ownernumber = '2349125932419' //ur owner number
-global.ownername = 'ᗰᗩᏀ€ᑎᎢᗩ' //ur owner name
+global.ownernumber = '2348107839861' //ur owner number
+global.ownername = 'ᏞᗩᔑᕼᎩ🦅🖤' //ur owner name
 global.websitex = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
 global.wagc = "https://chat.whatsapp.com/BTTuDZX787lLk6JmIEE71a"
-global.themeemoji = '😈'
+global.themeemoji = '🦅🖤'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD13' //script link
 global.packname = "Sticker By"
-global.author = "ᗰᗩᏀ€ᑎᎢᗩ\n\n+2349125932419"
-global.creator = "2349125932419@s.whatsapp.net"
+global.author = "ᗰᗩᏀ€ᑎᎢᗩ\n\n+ᏞᗩᔑᕼᎩ🦅🖤"
+global.creator = "2348107839861@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["2349125932419"] // Premium User
+global.premium = ["2348107839861"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v12' // menu type 'v1' => 'v12'
+global.typemenu = 'v8' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
 global.autoblocknumber = '212' //set autoblock country code
 global.antiforeignnumber = '212' //set anti foreign number country code
